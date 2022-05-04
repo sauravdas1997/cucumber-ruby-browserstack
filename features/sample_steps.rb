91e611b51801cac94ecc723612af515f7d0e8646
Saurav Das <saurav@Sauravs-MacBook-Pro.local>
2022-05-04 19:38:48 +0530

    locator variable changes

--- features/sample_steps.rb
+++ features/sample_steps.rb
@@ -1,33 +1,35 @@
 require 'rubygems'
 
+URL = 'https://www.flipkart.com'
+PROD_XPATH1 = '//div[@class="_4rR01T"]'
+PROD_XPATH2 = '//div[@class="_2B099V"]/a'
+SEARCHBOX_XPATH = '//input[@class="_3704LK"]'
+POPUP_ELEM_XPATH = '//button[@class="_2KpZ6l _2doB4z"]'
+
 Given('Text to seach is {string}') do |text_to_search|
   $driver.manage.timeouts.implicit_wait = 5
   $driver.manage.window.maximize
 
-  $driver.get("https://www.flipkart.com")
+  $driver.get(URL)
 
-  popup_elem_xpath = '//button[@class="_2KpZ6l _2doB4z"]'
-  popup_elem = $driver.find_element(:xpath, popup_elem_xpath)
+  popup_elem = $driver.find_element(:xpath, POPUP_ELEM_XPATH)
   popup_elem.click
-  
-  searchbox_xpath =  '//input[@class="_3704LK"]'
-  $driver.find_element(:xpath, searchbox_xpath).send_keys "#{text_to_search}"
+
+  $driver.find_element(:xpath, SEARCHBOX_XPATH).send_keys text_to_search.to_s
   $driver.action.send_keys(:return).perform
-  $driver.find_element(:xpath, searchbox_xpath).clear
+  $driver.find_element(:xpath, SEARCHBOX_XPATH).clear
 end
- 
+
 When('Text is searched, where type is {string}') do |type|
-  @prod_name = ""
-  sleep(3)
-  if type=='1'
-    prod_xpath = '//div[@class="_4rR01T"]'
-    @prod_name = $driver.find_element(:xpath, prod_xpath).text
-  else
-    prod_xpath = '//div[@class="_2B099V"]/a'
-    @prod_name = $driver.find_element(:xpath, prod_xpath).text
-  end
+  @prod_name = ''
+  sleep(2)
+  @prod_name = if type == '1'
+                 $driver.find_element(:xpath, PROD_XPATH1).text
+               else
+                 $driver.find_element(:xpath, PROD_XPATH2).text
+               end
 end
- 
+
 Then('I should get first produce name as {string}') do |expected|
   expect(@prod_name.class).to eq(expected.class)
   $driver.quit
